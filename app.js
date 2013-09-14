c2c42616200bab0241d4dbc2a4878cdc0a52a233
Juan David Millan <juanmillan85@gmail.com>
2013-09-14 15:00:27 +0100

    Changes in the server. using Express

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
  */
 
 var express = require('express'), 
-	routes = require('./routes'),
-	api = require('./routes/api'),
 	http = require('http'),
 	path = require('path');
 
@@ -12,13 +10,12 @@ var app = express();
 
 app.configure(function() {
 	app.set('port', process.env.PORT || 3000);
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
+	
 	app.use(express.favicon());
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
-	app.use(app.router);
+	
 	app.use(express.static(path.join(__dirname, 'public')));
 });
 
@@ -27,7 +24,7 @@ app.configure('development', function() {
 	app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+
 
 var server = http.createServer(app).listen(app.get('port'), function() {
 	console.log("Express server listening on port " + app.get('port'));
